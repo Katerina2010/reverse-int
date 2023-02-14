4b87505dbaaf044b427f73875d4394456ac880fa
Kate Bolshakova <katassr@mail.ru>
2023-02-14 15:07:41 +0300

    feat: change index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let x = Math.abs(n).toString()
+    let arr = []
+    for (let i = 0; i < x.length; i++) {
+        arr.unshift(x[i])
+    }
+    let str = arr.join("")
+    return Number(str)
 }
